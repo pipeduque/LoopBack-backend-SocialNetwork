f2810a62f899fb05bc6733e6fbd2f97403287acf
Hanner Obando <hannerobando@outlook.com>
2020-06-30 21:03:34 -0500

    coneccion a la base de datos, modelo user, repositorio, controlador

---
+++ src/models/user.model.ts
@@ -0,0 +1,83 @@
+import {Entity, model, property} from '@loopback/repository';
+
+@model()
+export class User extends Entity {
+  @property({
+    type: 'string',
+    id: true,
+  })
+  id?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  firstName: string;
+
+  @property({
+    type: 'string',
+  })
+  secondName?: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  surname: string;
+
+  @property({
+    type: 'string',
+  })
+  secondSurname?: string;
+
+  @property({
+    type: 'number',
+    required: true,
+  })
+  cellphone: number;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  gender: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  email: string;
+
+  @property({
+    type: 'string',
+    required: true,
+  })
+  password: string;
+
+  @property({
+    type: 'date',
+    required: true,
+  })
+  birthday: string;
+
+  @property({
+    type: 'string',
+  })
+  pathPhoto?: string;
+
+  @property({
+    type: 'array',
+    itemType: 'string',
+  })
+  prest?: string[];
+
+  constructor(data?: Partial<User>) {
+    super(data);
+  }
+}
+
+export interface UserRelations {
+  // describe navigational properties here
+}
+
+export type UserWithRelations = User & UserRelations;
