069ec2f3ba1cd4e6af3ac9f3f9a9567afb2e82d5
Hanner Obando <hannerobando@outlook.com>
2020-07-03 17:43:40 -0500

    Se agrega funcionalidad de seguimiento de seguidores

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -5,12 +5,20 @@ import {
   repository,
 } from '@loopback/repository';
 import {MongoDataSource} from '../datasources';
-import {Chat, Message, Publication, User, UserRelations, Owner, Ownerhasfollowers} from '../models';
+import {
+  Chat,
+  Message,
+  Owner,
+  Ownerhasfollowers,
+  Publication,
+  User,
+  UserRelations,
+} from '../models';
 import {ChatRepository} from './chat.repository';
 import {MessageRepository} from './message.repository';
-import {PublicationRepository} from './publication.repository';
 import {OwnerRepository} from './owner.repository';
 import {OwnerhasfollowersRepository} from './ownerhasfollowers.repository';
+import {PublicationRepository} from './publication.repository';
 
 export class UserRepository extends DefaultCrudRepository<
   User,
@@ -32,9 +40,15 @@ export class UserRepository extends DefaultCrudRepository<
     typeof User.prototype.id
   >;
 
-  public readonly owners: HasManyRepositoryFactory<Owner, typeof User.prototype.id>;
+  public readonly owners: HasManyRepositoryFactory<
+    Owner,
+    typeof User.prototype.id
+  >;
 
-  public readonly ownerhasfollowers: HasManyRepositoryFactory<Ownerhasfollowers, typeof User.prototype.id>;
+  public readonly ownerhasfollowers: HasManyRepositoryFactory<
+    Ownerhasfollowers,
+    typeof User.prototype.id
+  >;
 
   constructor(
     @inject('datasources.mongo') dataSource: MongoDataSource,
@@ -43,12 +57,27 @@ export class UserRepository extends DefaultCrudRepository<
     @repository.getter('ChatRepository')
     protected chatRepositoryGetter: Getter<ChatRepository>,
     @repository.getter('MessageRepository')
-    protected messageRepositoryGetter: Getter<MessageRepository>, @repository.getter('OwnerRepository') protected ownerRepositoryGetter: Getter<OwnerRepository>, @repository.getter('OwnerhasfollowersRepository') protected ownerhasfollowersRepositoryGetter: Getter<OwnerhasfollowersRepository>,
+    protected messageRepositoryGetter: Getter<MessageRepository>,
+    @repository.getter('OwnerRepository')
+    protected ownerRepositoryGetter: Getter<OwnerRepository>,
+    @repository.getter('OwnerhasfollowersRepository')
+    protected ownerhasfollowersRepositoryGetter: Getter<
+      OwnerhasfollowersRepository
+    >,
   ) {
     super(User, dataSource);
-    this.ownerhasfollowers = this.createHasManyRepositoryFactoryFor('ownerhasfollowers', ownerhasfollowersRepositoryGetter,);
-    this.registerInclusionResolver('ownerhasfollowers', this.ownerhasfollowers.inclusionResolver);
-    this.owners = this.createHasManyRepositoryFactoryFor('owners', ownerRepositoryGetter,);
+    this.ownerhasfollowers = this.createHasManyRepositoryFactoryFor(
+      'ownerhasfollowers',
+      ownerhasfollowersRepositoryGetter,
+    );
+    this.registerInclusionResolver(
+      'ownerhasfollowers',
+      this.ownerhasfollowers.inclusionResolver,
+    );
+    this.owners = this.createHasManyRepositoryFactoryFor(
+      'owners',
+      ownerRepositoryGetter,
+    );
     this.registerInclusionResolver('owners', this.owners.inclusionResolver);
     this.messages = this.createHasManyRepositoryFactoryFor(
       'messages',
